52d42dbaea69598fad64b56de57bb3c08fc82d30
evgeni <9232485@gmail.com>
2023-06-16 23:09:00 +0300

    homework-1 (fixed_1)

--- src/channel.py
+++ src/channel.py
@@ -7,15 +7,16 @@ api_key: str = os.getenv('YT_API_KEY')
 
 class Channel:
     """Класс для ютуб-канала"""
-    # создать специальный объект для работы с API
+
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+
         self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
+        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         return print(channel)
