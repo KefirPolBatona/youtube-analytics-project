442b66d98069cd4a8a59364aaf5fbb27c3cc52f4
evgeni <9232485@gmail.com>
2023-06-16 23:04:51 +0300

    homework-1

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,4 +1,7 @@
+##############
+
 from src.channel import Channel
+
 
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
@@ -56,3 +59,5 @@ if __name__ == '__main__':
 }
 
     """
+
+#######################
