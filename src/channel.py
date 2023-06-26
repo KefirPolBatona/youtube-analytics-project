de3635e9003cd6a006ea8b494bafb2928bd7b1d1
evgeni <9232485@gmail.com>
2023-06-26 19:32:52 +0300

    homework-2. I have a problem. This task is performed incorrectly in the module main.py:
    # менять не можем
    moscow python.__channel_id = 'Новое название'
    # AttributeError: property 'channel_id' of 'Channel' object has no setter
    
    My code doesn't get the specified error, instead it creates a new attribute: __channel_id = 'Новое название'

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from googleapiclient.discovery import build
 
@@ -6,17 +7,48 @@ api_key: str = os.getenv('YT_API_KEY')
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """Класс для ютуб-канала.
+    Принимает id канала.
+    Определяет аргументы:
+        self.channel - общая информация о канале
+        self.title - название канала
+        self.info - описание канала
+        self.url - вэб-ссылка на канал
+        self.subscribers_count - количество подписчиков
+        self.video_count - количество видео
+        self.views_count - общее количество просмотров всех видео.
+    """
 
+    # Специальный объект для работы с API
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализируется по id канала. Дальше все данные будут подтягиваться по API."""
 
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+
+        self.channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.info = self.channel["items"][0]["snippet"]["description"]
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
+        self.subscribers_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.views_count = self.channel["items"][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
+        """Выводит в консоль общую информацию о канале."""
 
-        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        return print(channel)
+        return print(self.channel)
+
+    def to_json(self, json_name=''):
+        """Создает файл moscowpython.json и сохраняет в нем значения атрибутов экземпляра"""
+
+        with open(json_name, "w", encoding="utf-8") as write_file:
+            return json.dump(self.__dict__, write_file, indent=2, ensure_ascii=False)
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API вне класса"""
+
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
